48023a4b32bf02ba0dd0826e2090aa533376b2e2
dean <deanlloyd64@gmail.com>
2020-07-19 14:08:57 -0400

    fill contactBurb with written copy

---
+++ src/Components/contactBlurb.js
@@ -0,0 +1,17 @@
+export const contactBlurn = () => {
+    return(
+        <div>
+            <h2>Get in Touch</h2>
+            <p>
+                I'm interested in medium to long-term contracts
+                and full-time opportunities. However, if you 
+                have any other request feel free to contact me.
+            </p>
+            <ul>
+                <li>LinkedIn</li>
+                <li>GitHub</li>
+                <li>Twitter</li>
+            </ul>
+        </div>
+    )
+}
