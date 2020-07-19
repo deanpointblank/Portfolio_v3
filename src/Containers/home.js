bfc067aa5a66420987c11c8ae6ff76bcc67216aa
dean <deanlloyd64@gmail.com>
2020-07-19 16:59:23 -0400

    install react-bootstrap

--- src/Containers/home.js
+++ src/Containers/home.js
@@ -8,7 +8,7 @@ export default class HomeContainer extends Component {
     render(){
         return(
             <div>
-                <p>This is the Home page</p>
+                <p>This is the Home page<span class="badge bg-secondary">New</span></p>
                 <HomeAbout />
                 <HomeSkills />
             </div>
