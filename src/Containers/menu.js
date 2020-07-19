5899f92aebd65d9ea32d4a94ed4a7f67321ac77c
dean <deanlloyd64@gmail.com>
2020-07-19 13:34:45 -0400

    add dummy text to pages, write logic for react router

---
+++ src/Containers/menu.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class MenuContainer extends Component {
+
+    render(){
+        return(
+            <div>
+                <p>This is the menu</p>
+            </div>
+        )
+    }
+}
