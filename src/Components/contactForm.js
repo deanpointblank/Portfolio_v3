c8eb130107479d8f345dcb4c2132370e9c516ccd
dean <deanlloyd64@gmail.com>
2020-07-19 15:21:00 -0400

    add basic form to contactForm

---
+++ src/Components/contactForm.js
@@ -0,0 +1,11 @@
+export const contactForm = () => {
+    return(
+        <form>
+            <input type="text" name="name" placeholder="Name" id="name" value=""></input>
+            <input type="email" name="email" placeholder="Email" id="email" value=""></input>
+            <input type="text" name="subject" placeholder="Subject" id="subject" value=""></input>
+            <textarea type="text" name="message" placeholder="Message" id="message" value=""></textarea>
+            <input type="submit" value="send">
+        </form>
+    )
+}
