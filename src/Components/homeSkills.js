a4e24003d419b06e43f26812a41eeb670bec8432
dean <deanlloyd64@gmail.com>
2020-07-20 13:22:10 -0400

    style skills section: place skills in rows and 'justify-content-betwen'

--- src/Components/homeSkills.js
+++ src/Components/homeSkills.js
@@ -5,36 +5,42 @@ export const HomeSkills = () => {
     return(
         <div className="container">
             <Row>
-                <h1 className="text-center">Here are my skills:</h1>
+                <Col className="d-flex justify-content-center">
+                    <h1>Here are my skills:</h1>
+                </Col>
             </Row>
 
-            <Row className="display-4 text-center">
-                    <div>
+            <Row className="display-4 h-50">
+                <Col className="w-100 d-flex justify-content-between">
+                    <div className="d-inline">
                         Rails
                     </div>
-                    <div>
+                    <div className="d-inline">
                         Html5
                     </div>
-                    <div>
+                    <div className="d-inline">
                         Redux
                     </div>
-                    <div>
+                    <div className="d-inline">
                         Ruby
                     </div>
+                </Col>
             </Row>
-            <Row className="display-4 text-center">
-                    <div>
+            <Row className="display-4 h-50">
+                <Col className="w-100 d-flex justify-content-between">
+                    <div className="d-inline">
                         React
                     </div>
-                    <div>
+                    <div className="d-inline">
                         JavaScript
                     </div>
-                    <div>
+                    <div className="d-inline">
                         SQL
                     </div>
-                    <div>
+                    <div className="d-inline">
                         Bootstrap
                     </div>
+                </Col>
             </Row>
         </div>
     )
