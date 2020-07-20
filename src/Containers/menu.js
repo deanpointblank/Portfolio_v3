abbde04464d129fb53c780bd4d189df503ef0f28
dean <deanlloyd64@gmail.com>
2020-07-19 21:16:16 -0400

    alter menu logo to be more prominent

--- src/Containers/menu.js
+++ src/Containers/menu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 
 export default class MenuContainer extends Component {
@@ -6,7 +7,7 @@ export default class MenuContainer extends Component {
     render(){
         return(
             <Nav defaultActiveKey="/" className="flex-column">
-                    <Nav.Link href="/">Dean Betty</Nav.Link>
+                    <Navbar.Brand href="/">Dean Betty</Navbar.Brand>
                     <Nav.Link href="/projects">Projects</Nav.Link>
                     <Nav.Link href="/blog">Blog</Nav.Link>
                     <Nav.Link href="/contact">Contact</Nav.Link>
