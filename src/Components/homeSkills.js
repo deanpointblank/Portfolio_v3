ba584ca18f7ba7fcc7267b64db20f5b9c3d6ab13
dean <deanlloyd64@gmail.com>
2020-07-20 13:42:31 -0400

    basic css completed for home page

--- src/Components/homeSkills.js
+++ src/Components/homeSkills.js
@@ -4,40 +4,40 @@ import { Row, Col } from 'react-bootstrap';
 export const HomeSkills = () => {
     return(
         <div className="container">
-            <Row>
+            <Row className="h-25">
                 <Col className="d-flex justify-content-center">
-                    <h1>Here are my skills:</h1>
+                    <h1 className="display-3">Here are my skills:</h1>
                 </Col>
             </Row>
 
-            <Row className="display-4 h-50">
-                <Col className="w-100 d-flex justify-content-between">
-                    <div className="d-inline">
+            <Row className="display-4 h-25">
+                <Col className="w-100 d-flex justify-content-center">
+                    <div className="d-inline w-25">
                         Rails
                     </div>
-                    <div className="d-inline">
+                    <div className="d-inline w-25">
                         Html5
                     </div>
-                    <div className="d-inline">
+                    <div className="d-inline w-25">
                         Redux
                     </div>
-                    <div className="d-inline">
+                    <div className="d-inline w-25">
                         Ruby
                     </div>
                 </Col>
             </Row>
-            <Row className="display-4 h-50">
-                <Col className="w-100 d-flex justify-content-between">
-                    <div className="d-inline">
+            <Row className="display-4 h-25">
+                <Col className="w-100 d-flex justify-content-center">
+                    <div className="d-inline w-25">
                         React
                     </div>
-                    <div className="d-inline">
+                    <div className="d-inline w-25">
                         JavaScript
                     </div>
-                    <div className="d-inline">
+                    <div className="d-inline w-25">
                         SQL
                     </div>
-                    <div className="d-inline">
+                    <div className="d-inline w-25">
                         Bootstrap
                     </div>
                 </Col>
