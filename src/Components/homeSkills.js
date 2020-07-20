0132d2943c97a5911b9317f96a53508f2b269827
dean <deanlloyd64@gmail.com>
2020-07-20 12:47:24 -0400

    begin stlying display for skills on home page

--- src/Components/homeSkills.js
+++ src/Components/homeSkills.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import { Card, CardColumns } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 export const HomeSkills = () => {
     return(
-        <div>
-            <h1 className="text-center">Here are my skills:</h1>
+        <div className="container">
+            <Row>
+                <h1 className="text-center">Here are my skills:</h1>
+            </Row>
 
-            <ul>
-                <li>Rails</li>
-                <li>Html5</li>
-                <li>Redux</li>
-                <li>Ruby</li>
-                <li>React</li>
-                <li>JavaScript</li>
-                <li>SQL</li>
-                <li>Bootstrap</li>
-            </ul>
+            <Row className="display-4 text-center">
+                    <div>
+                        Rails
+                    </div>
+                    <div>
+                        Html5
+                    </div>
+                    <div>
+                        Redux
+                    </div>
+                    <div>
+                        Ruby
+                    </div>
+            </Row>
+            <Row className="display-4 text-center">
+                    <div>
+                        React
+                    </div>
+                    <div>
+                        JavaScript
+                    </div>
+                    <div>
+                        SQL
+                    </div>
+                    <div>
+                        Bootstrap
+                    </div>
+            </Row>
         </div>
     )
 }
