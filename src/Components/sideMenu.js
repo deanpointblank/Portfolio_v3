957c5bd0af0ae01841fe6afb00a1e16e27faa66c
dean <deanlloyd64@gmail.com>
2020-07-22 14:33:45 -0400

    remove react-buger-menu, create sideMenu Component

---
+++ src/Components/sideMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Row, Nav, Navbar } from 'react-bootstrap';
+
+export default class MenuContainer extends Component {
+
+    render(){
+        return(
+            <>
+
+            </>
+        )
+    }
+}
