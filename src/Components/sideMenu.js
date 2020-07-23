e443c5cb1ecd6b2080f8ce66c0ec116e13e0a2b9
dean <deanlloyd64@gmail.com>
2020-07-22 22:34:44 -0400

    replace non alpha numeric chars in url links

--- src/Components/sideMenu.js
+++ src/Components/sideMenu.js
@@ -15,12 +15,12 @@ export default class SideMenu extends Component {
 
     render(){
         return(
-            <Navbar bg="light" expand="lg" defaultActiveKey="/" className="flex-column sideMenu">
+            <Navbar bg="light" expand="lg" defaultActiveKey="/" className="flex-column">
             {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
             {/* <Navbar.Collapse id="basic-navbar-nav"> */}
                 <Nav className="mr-auto flex-column">
                     {this.props.menuList.map(post => {
-                       return <Nav.Link className="underlined" href={`/blog/${post.Title.split(" ").join("_")}`}>{post.Title}</Nav.Link>
+                       return <Nav.Link className="underlined sideMenu" href={`/blog/${post.Title.replace(/[^0-9a-z]/gi, '_')}`}>{post.Title}</Nav.Link>
                     })}
                 </Nav>
             {/* </Navbar.Collapse> */}
